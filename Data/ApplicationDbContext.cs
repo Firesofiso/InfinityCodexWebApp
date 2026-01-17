94909781f34bddc41e874e5c70e4cf119a48144c
Harry Bayley <harry.bayley@gmail.com>
2026-01-16 17:25:35 -0800

    Add user entity model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,5 +5,6 @@ namespace InfinityCodexWebApp.Data;
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : DbContext(options)
 {
+    public DbSet<User> Users => Set<User>();
     public DbSet<WeatherForecast> WeatherForecasts => Set<WeatherForecast>();
 }
